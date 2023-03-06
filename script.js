f08ee0619eaf1c35199771055c7d59b0c857d7cf
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-06 15:36:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+ function calculateSum() {
+        const num1 = parseFloat(document.getElementById("n1").value);
+        const num2 = parseFloat(document.getElementById("n2").value);
+        const sum = num1 + num2;
+        document.getElementById("sum").innerHTML = `The sum is ${sum}`;
+      }
